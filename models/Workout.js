d64eae4e9a7202097d884f1767fdc4fc50a75ec8
kwu3 <kwu3@villanova.edu>
2020-05-19 16:59:32 -0400

    Created workout schema

---
+++ models/Workout.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now,
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        enum: ["Cardio", "Resistance"],
+        required: "Exercise type must be Cardio or Resistance"
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: "Exercise Name is Required",
+      },
+      duration: {
+        type: Number,
+        trim: true,
+      },
+      weight: {
+        type: Number,
+        trim: true,
+      },
+      reps: {
+        type: Number,
+        trim: true,
+      },
+      sets: {
+        type: Number,
+        trim: true,
+      },
+      duration: {
+        type: Number,
+        trim: true,
+      }
+    }
+  ]
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
