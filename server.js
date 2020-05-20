10c507d11d3dc6383eed7fc8e2177e5852491f1c
kwu3 <kwu3@villanova.edu>
2020-05-20 15:34:22 -0400

    Added get and put routes for the workouts

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
-// const db = require("./models");
+const db = require("./models");
 
 const app = express();
 
@@ -18,6 +18,39 @@ app.use(express.static("public"));
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
 });
+
+// Routes
+app.get("/api/workouts", async (req, res) => {
+  try {
+    const allWorkouts = await db.Workout.find({});
+    res.json(allWorkouts);
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+// app.put("/api/workouts", async (req, res) => {
+//   try {
+//     const newExercise = await db.Workout.update(req.body);
+//     res.json(newExercise);
+//   }
+// })
+app.put("/api/workouts/:id", (req, res) => {
+  db.Workout.update(
+    { _id: req.params.id },
+    {
+      $push: { exercises: req.body },
+    },
+    (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.json(data);
+      }
+    }
+  );
+});
+
 
 // Start the server
 app.listen(PORT, () => {
