f59b0f76ca13880e77872e410240cad7c6e4cab9
kwu3 <kwu3@villanova.edu>
2020-05-20 17:26:57 -0400

    Created html routes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
@@ -13,13 +14,26 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "/public")));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
 });
 
 // Routes
+// *** HTML ***
+app.get("/", (req,res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/exercise", (req,res) => {
+  res.sendFile(path.join(__dirname, "public", "exercise.html"));
+});
+
+app.get("/stats", (req,res) => {
+  res.sendFile(path.join(__dirname, "public", "stats.html"));
+});
+
 app.get("/api/workouts", (req, res) => {
   db.Workout.find({}, (err, data) => {
     if (err) {
@@ -30,6 +44,7 @@ app.get("/api/workouts", (req, res) => {
   });
 });
 
+// *** API ***
 app.put("/api/workouts/:id", (req, res) => {
   db.Workout.update(
     { _id: req.params.id },
